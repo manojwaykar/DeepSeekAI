8e945813e4ed28b602a83659d3f4d0b4f87f434c
Manoj Waykar <waykarmanoj012@gmail.com>
2025-09-27 16:58:21 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ mongoose.connect(MONGO_URL,  {
 })
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://deepseekai-frontend-772r.onrender.com",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
